6aef3e03a9e99a5cb5dd92400efb6a146b4cd786
Falgun <77203052+fxlgun@users.noreply.github.com>
2024-03-14 15:25:49 +0530

    Update scraper.js

--- client/src/utils/scraper.js
+++ client/src/utils/scraper.js
@@ -1,21 +1,8 @@
 
 
 export const scrapeAmazonPrice = async (url) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-        "url": url
-    });
-
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
-
-    const response = await fetch("https://costeye.vercel.app/scrape", requestOptions)
+    const response = await fetch("https://costeye.vercel.app/scrape", { method: "POST", body: { url } })
         .then((response) => response.json())
 
     return response.price
