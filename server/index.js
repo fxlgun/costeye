10ac1e7d4dba6e051734ddd352cbf03250b690d2
Falgun <77203052+fxlgun@users.noreply.github.com>
2024-03-14 15:19:32 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const { scrapeAmazonPrice } = require('./scrape');
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.header({"Access-Control-Allow-Origin": "*"});
+  next();
+}) 
 
 
 // Routes
